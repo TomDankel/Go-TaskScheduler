3ea7985d468c942eb3bca5c23a910c336705848f
Luca-Schwarz <lucky.f-black@web.de>
2023-01-27 18:04:09 +0100

    Implement thread as interface

--- Thread/thread.go
+++ Thread/thread.go
@@ -1,5 +1,13 @@
 package thread
 
+type Multithreading interface {
+	Continue()
+	Suspend()
+	Play(func(chan int, chan int, chan int))
+	Kill()
+	Control() int
+}
+
 type Thread struct {
 	SuspendCh chan int
 	PlayCh    chan int
@@ -9,7 +17,7 @@ type Thread struct {
 func (th *Thread) Play(method func(suspend chan int, play chan int, kill chan int)) {
 	go method(th.SuspendCh, th.PlayCh, th.KillCh)
 }
-func (th *Thread) Restart() {
+func (th *Thread) Continue() {
 	th.PlayCh <- 1
 }
 func (th *Thread) Suspend() {
